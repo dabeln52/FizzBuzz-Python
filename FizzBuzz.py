bcfb65aadebc406ca860fbb3c2e2c386dcd248e0
Derek Abeln <111763839+dabeln52@users.noreply.github.com>
2023-03-09 09:44:57 -0600

    Error in first two attemps. Now preforms!

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,9 +1,9 @@
 for num in range(1,101):
-    output = ''
-if num % 3 == 0:
-    output += "Fizz"
-if num % 5 == 0:
-    output += "Buzz"
-if not output:
-    output = num
-print(output)
+    if num % 3 == 0 and num % 5 == 0:
+        print( num, "FizzBuzz")
+    elif num % 3 == 0:
+        print(num , "Fizz")
+    elif num % 5 == 0:
+        print(num ,"Buzz")
+    else:
+        print(num)
