53d6de8973ddc9e4c4213b6163fd9419f4ddf626
Derek Abeln <111763839+dabeln52@users.noreply.github.com>
2023-03-09 09:38:57 -0600

    Tried new style to get ride of duplicate prints

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,9 +1,9 @@
 for num in range(1,101):
-    if num % 3 == 0 and num % 5 == 0:
-        print("FizzBuzz")
-    elif num % 3 == 0:
-        print("Fizz")
-    elif num % 5 == 0:
-        print("Buzz")
-    else:
-        print(num)
+    output = ''
+if num % 3 == 0:
+    output += "Fizz"
+if num % 5 == 0:
+    output += "Buzz"
+if not output:
+    output = num
+print(output)
